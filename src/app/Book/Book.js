7ef8da4dcd79a3f5d68f479af71d41ec42e90a49
svignesh93 <vignesh.vic727@gmail.com>
2022-07-03 12:13:31 +0530

    Created book component

---
+++ src/app/Book/Book.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Book.css";
+
+class Book extends React.Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+    onBookShelfChange: PropTypes.func.isRequired,
+  };
+
+  render() {
+    const { book, onBookShelfChange } = this.props;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book.imageLinks.thumbnail})`,
+            }}
+          ></div>
+          <div className="book-shelf-changer">
+            <select
+              value={book.shelf}
+              onChange={(e) => onBookShelfChange(book, e.target.value)}
+            >
+              <option value="move" disabled>
+                Move to...
+              </option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors.map((author) => (
+          <div key={author} className="book-authors">
+            {author}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Book;
--- src/app/bookshelf/BookShelf.js
+++ src/app/bookshelf/BookShelf.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Book from "../Book/Book";
 import "./BookShelf.css";
 
 class BookShelf extends React.Component {
@@ -19,42 +20,7 @@ class BookShelf extends React.Component {
           <ol className="books-grid">
             {books.map((book) => (
               <li key={book.id}>
-                <div className="book">
-                  <div className="book-top">
-                    <div
-                      className="book-cover"
-                      style={{
-                        width: 128,
-                        height: 193,
-                        backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                      }}
-                    ></div>
-                    <div className="book-shelf-changer">
-                      <select
-                        value={book.shelf}
-                        onChange={(e) =>
-                          onBookShelfChange(book, e.target.value)
-                        }
-                      >
-                        <option value="move" disabled>
-                          Move to...
-                        </option>
-                        <option value="currentlyReading">
-                          Currently Reading
-                        </option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">{book.title}</div>
-                  {book.authors.map((author) => (
-                    <div key={author} className="book-authors">
-                      {author}
-                    </div>
-                  ))}
-                </div>
+                <Book book={book} onBookShelfChange={onBookShelfChange} />
               </li>
             ))}
           </ol>
